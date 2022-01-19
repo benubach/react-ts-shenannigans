e5bf7f225235f5e2aca1bc217c8054233f5539ba
benubach <benubach@gmail.com>
2022-01-19 12:08:12 -0600

    1 file modified

--- components/App.tsx
+++ components/App.tsx
@@ -9,23 +9,44 @@ import { getUsers, IUser } from './api';
 
 const App = () => {
   const [users, setUsers] = useState<Array<IUser>>([]);
-  const userCount = useRef(0);
+  const [intervalId, setIntervalId] = useState<ReturnType<
+    typeof setInterval
+  > | null>(null);
+  const [userCount, setUserCount] = useState(0);
 
   useEffect(() => {
     (async () => {
-      const res = await getUsers();
-      setUsers(res);
-      setInterval(() => {
-        userCount.current++;
-      }, 1500);
+      setUsers(await getUsers());
+      setIntervalId(
+        setInterval(() => {
+          setUserCount((prev) => {
+            return ++prev;
+          });
+        }, 1500)
+      );
     })();
+    return () => {
+      if (intervalId) {
+        clearInterval(intervalId);
+      }
+    };
   }, []);
+  useEffect(() => {
+    if (intervalId && userCount >= users.length) {
+      clearInterval(intervalId);
+      setIntervalId(null);
+    }
+  }, [userCount, intervalId !== null]);
+
   return (
     <>
       <h1>Users:</h1>
+      <h2>
+        {userCount} - {`${intervalId}`}
+      </h2>
       <ul>
         {users.map((user, index) =>
-          index < userCount.current ? (
+          index < userCount ? (
             <li key={`${user.id}.${user.name}`}>
               {user.id} - {user.name}{' '}
             </li>
